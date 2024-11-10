c4da8985ef833453d255e5021339cb2cd8f74781
yangyioryy <yangyioryy@qq.com>
2024-11-10 22:07:13 +0800

    修改用户信息

--- src/Persistence/DBUtil.java
+++ src/Persistence/DBUtil.java
@@ -4,7 +4,7 @@ import java.sql.*;
 
 public class DBUtil {
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String URL = "jdbc:mysql://localhost:3306/demo?useSSL=false&serverTimezone=Asia/Shanghai";
+    private static final String URL = "jdbc:mysql://localhost:3306/demo?useSSL=false&serverTimezone=Asia/Shanghai&useUnicode=true&characterEncoding=UTF-8";
 
     private static final String USERNAME = "root";
     private static final String PASSWORD = "12345678";
